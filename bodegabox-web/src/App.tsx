e3e985448901c4b430120b927ec40f47f8aab61e
Luka Eaton <eatonluka@gmail.com>
2025-08-30 16:25:23 -0400

    final blank pages integration

--- bodegabox-web/src/App.tsx
+++ bodegabox-web/src/App.tsx
@@ -3,14 +3,17 @@ import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 import { Navigation } from "./components/";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { ShoppingList } from "./views/";
+import { ShoppingListPage, RecipesPage, ExpensesPage, SettingsPage } from "./views/";
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
       <Navigation />
       <Routes>
-        <Route path="/list" element={<ShoppingList />} />
+        <Route path="/list" element={<ShoppingListPage />} />
+        <Route path="/recipes" element={<RecipesPage />} />
+        <Route path="/expenses" element={<ExpensesPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
         <Route path="*" element={<Navigate to="/list" replace />} />
       </Routes>
     </MantineProvider>
--- bodegabox-web/src/views/ShoppingList.tsx
+++
@@ -1,5 +0,0 @@
-export function ShoppingList() {
-	return (
-        <div>lists</div>
-	);
-}
