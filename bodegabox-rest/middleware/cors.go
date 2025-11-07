f60df28075ec23c3e3c68d394375423e2f974316
Luka Eaton <eatonluka@gmail.com>
2025-11-07 13:44:26 -0500

    setup nginx for prod only

--- bodegabox-rest/middleware/cors.go
+++ bodegabox-rest/middleware/cors.go
@@ -3,18 +3,22 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
+	env := os.Getenv("ENV")
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		if env == "development" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
-		if c.Request.Method == http.MethodOptions {
-			c.AbortWithStatus(http.StatusNoContent)
-			return
+			if c.Request.Method == http.MethodOptions {
+				c.AbortWithStatus(http.StatusNoContent)
+				return
+			}
 		}
 
 		c.Next()
